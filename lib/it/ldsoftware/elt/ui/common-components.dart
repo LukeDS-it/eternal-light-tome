7fd3a225ae584f500c16813be4dff0aa6636ad84
LukeDS-it <luca.mcgyver@gmail.com>
2019-06-28 10:47:15 +0200

    end story view design

--- lib/it/ldsoftware/elt/ui/common-components.dart
+++ lib/it/ldsoftware/elt/ui/common-components.dart
@@ -11,60 +11,71 @@ class CampaignDrawer extends Drawer {
             ),
             ListTile(
               title: Text("Story"),
-              leading: Icon(Icons.book),
+              leading: CommonIcons.STORY_ICON,
               onTap: () {
                 print("hello");
               },
             ),
             ListTile(
               title: Text("Characters"),
-              leading: Icon(Icons.contact_mail),
+              leading: CommonIcons.CHARACTER_ICON,
               onTap: () {
                 print("hello");
               },
             ),
             ListTile(
               title: Text("Places"),
-              leading: Icon(Icons.pin_drop),
+              leading: CommonIcons.PLACE_ICON,
               onTap: () {
                 print("hello");
               },
             ),
             ListTile(
               title: Text("Items"),
-              leading: Icon(Icons.move_to_inbox),
+              leading: CommonIcons.ITEM_ICON,
               onTap: () {
                 print("hello");
               },
             ),
             ListTile(
               title: Text("Quests"),
-              leading: Icon(Icons.help),
+              leading: CommonIcons.QUEST_ICON,
               onTap: () {
                 print("hello");
               },
             ),
             ListTile(
               title: Text("Monsters"),
-              leading: Icon(Icons.warning),
+              leading: CommonIcons.MONSTER_ICON,
               onTap: () {
                 print("hello");
               },
             ),
             ListTile(
               title: Text("Folks"),
-              leading: Icon(Icons.people),
+              leading: CommonIcons.FOLK_ICON,
               onTap: () {
                 print("hello");
               },
             ),
             ListTile(
               title: Text("Notes"),
-              leading: Icon(Icons.speaker_notes),
+              leading: CommonIcons.NOTE_ICON,
               onTap: () {
                 print("hello");
               },
             ),
           ],
         ));
+}
+
+class CommonIcons {
+  static const STORY_ICON = Icon(Icons.book);
+  static const CHARACTER_ICON = Icon(Icons.person);
+  static const PLACE_ICON = Icon(Icons.pin_drop);
+  static const ITEM_ICON = Icon(Icons.vpn_key);
+  static const QUEST_ICON = Icon(Icons.help);
+  static const MONSTER_ICON = Icon(Icons.warning);
+  static const FOLK_ICON = Icon(Icons.people);
+  static const NOTE_ICON = Icon(Icons.speaker_notes);
 }
--- lib/it/ldsoftware/elt/ui/story-view.dart
+++ lib/it/ldsoftware/elt/ui/story-view.dart
@@ -17,11 +17,27 @@ class StoryView extends StatefulWidget {
 class _StoryState extends State<StoryView> {
   final String _campaignName;
   List<ChapterVM> chapters = [
-    ChapterVM("Home", """
+    ChapterVM(
+        "Home",
+        """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non eros a ex malesuada laoreet dignissim sed tellus. Nunc blandit feugiat mollis. Quisque dapibus leo sit amet ullamcorper ultricies. Pellentesque nisl sapien, tempus quis libero in, blandit aliquet erat. Suspendisse pellentesque mattis fermentum. Proin fermentum euismod ligula, nec congue libero sagittis sed. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris ac vehicula risus. Aliquam lobortis vulputate felis vitae dignissim. Nam ullamcorper turpis quis ipsum pellentesque convallis. Nullam cursus volutpat urna, a rhoncus est cursus eget. Quisque hendrerit orci eu accumsan iaculis. Fusce quam enim, malesuada eu libero sed, tempor imperdiet leo. Pellentesque et semper est. Nunc rhoncus magna ex, non malesuada metus lacinia ut.
 
 xIn hac habitasse platea dictumst. Vestibulum semper congue ultricies. Pellentesque elementum facilisis suscipit. Ut efficitur, nibh vitae euismod fringilla, ipsum dolor sollicitudin leo, in sodales purus lectus egestas arcu. Vestibulum eget turpis porttitor, sodales diam non, tincidunt tortor. Vestibulum blandit consequat mattis. Ut maximus vulputate mi. Pellentesque mattis, justo a tristique fermentum, dui dolor mollis dui, non ultricies tortor risus et tortor. Nulla facilisi.
-    """)
+    """,
+        ["Cane"],
+        null,
+        null,
+        null,
+        ["Cane"]),
+    ChapterVM(
+        "Taverna polena spezzata",
+        "Andiamo nella taverna, troviamo un tizio che ci da una missione",
+        ["Giovanni"],
+        ["Palude di Ipswitch"],
+        ["Porta la scatola"],
+        ["Elfi"],
+        ["Velociragno"],
+        ["Scatola"])
   ];
 
   _StoryState(this._campaignName);
@@ -40,30 +56,32 @@ xIn hac habitasse platea dictumst. Vestibulum semper congue ultricies. Pellentes
       ),
       drawer: CampaignDrawer(_campaignName),
       body: ListView.builder(
-        padding: EdgeInsets.zero,
+          padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
           itemCount: chapters.length,
           itemBuilder: (context, position) {
             var chapter = chapters[position];
+            var story = chapter.story.length > 300
+                ? chapter.story.substring(0, 300) + "..."
+                : chapter.story;
             return GestureDetector(
-              onTap: () {
-                print("Open full chapter view");
-              },
-              child: Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    ListTile(
-                      title: Text(chapter.place),
-                      subtitle: Text(chapter.story.substring(0, 300) + "..."),
-                      isThreeLine: true,
-                    ),
-                    Row(
-
-                    ),
-                  ],
-                ),
-              )
-            );
+                onTap: () {
+                  print("Open full chapter view");
+                },
+                child: Card(
+                  elevation: 5,
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(chapter.place),
+                        subtitle: Text(story),
+                        isThreeLine: true,
+                      ),
+                      Wrap(
+                        children: _getChips(chapter),
+                      ),
+                    ],
+                  ),
+                ));
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
@@ -71,5 +89,34 @@ xIn hac habitasse platea dictumst. Vestibulum semper congue ultricies. Pellentes
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
+  }
+
+  List<Widget> _getChips(ChapterVM chapter) {
+    return <Widget>[
+      if (chapter.people != null)
+        Chip(
+            label: Text(chapter.people.length.toString()),
+            avatar: CommonIcons.CHARACTER_ICON),
+      if (chapter.places != null)
+        Chip(
+            label: Text(chapter.places.length.toString()),
+            avatar: CommonIcons.PLACE_ICON),
+      if (chapter.quests != null)
+        Chip(
+            label: Text(chapter.quests.length.toString()),
+            avatar: CommonIcons.QUEST_ICON),
+      if (chapter.folks != null)
+        Chip(
+            label: Text(chapter.folks.length.toString()),
+            avatar: CommonIcons.FOLK_ICON),
+      if (chapter.monsters != null)
+        Chip(
+            label: Text(chapter.monsters.length.toString()),
+            avatar: CommonIcons.MONSTER_ICON),
+      if (chapter.items != null)
+        Chip(
+            label: Text(chapter.items.length.toString()),
+            avatar: CommonIcons.ITEM_ICON),
+    ];
   }
 }
